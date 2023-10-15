bfcb68e96001472f4ff4b6975494df9b8b1df38e
Alenae <alena_trsenkov@list.ru>
2023-10-16 01:02:46 +0300

    написан финальный код

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.NetworkInformation;
+
+public class Program
+{
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Введите цифру для обозначения дня недели:");
+        int day=Convert.ToInt32(Console.ReadLine());
+        if (day>7)
+        {
+            Console.WriteLine("Такого дня недели не существует");
+        }
+        else if (IsWeekend(day))
+        {
+            Console.WriteLine("Этот день является выходным");
+        }
+        else
+        {
+            Console.WriteLine("Этот день не является выходным");
+        }
+    }
+    public static bool IsWeekend(int day)
+    {
+        return day ==6||day==7;
+
+    }
+    
+}
